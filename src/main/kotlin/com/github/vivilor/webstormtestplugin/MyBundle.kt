ca43e95997113bc27d079170f7390b6e272cfb24
GitHub Action <action@github.com>
2021-07-24 11:40:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/vivilor/webstormtestplugin/MyBundle.kt
+++ src/main/kotlin/com/github/vivilor/webstormtestplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vivilor.webstormtestplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/vivilor/webstormtestplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vivilor.webstormtestplugin.services
+
+import com.github.vivilor.webstormtestplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
